ef410e3388cc969750e07db91a5430d3187d3cd1
Adiseshu18 <adiseshu503@gmail.com>
2025-02-21 12:25:39 +0530

    task added

---
+++ App.js
@@ -0,0 +1,77 @@
+
+function App() {
+  return (
+    <div>
+      <p>
+        <h1>Hello World</h1>
+      </p>
+      <form>
+        <h1>Login Form</h1>
+        <label>Username:</label>
+        <input type="text" placeholder="Enter username"></input><br></br><br></br>
+        <label>Email:</label>
+        <input type="email" placeholder="enter email"></input><br></br><br></br>
+        <label>Password:</label>
+        <input type="password" placeholder="enter password"></input><br></br><br></br>
+        <button >Submit</button>
+      </form>
+     <h1>Welcome To My Page</h1>
+     <p>Yeah I'm Adi Thank for choosing my web page</p>
+
+     <h2>About</h2>
+     <p>This page is exploring the new things with me.</p>
+
+     <section>
+      <h2>Services</h2>
+      <ul>
+        <li>Web Development</li>
+        <li>App Design</li>
+        <li>Consulting</li>
+      </ul>
+    </section>
+<br></br><br></br>
+<table>
+  <thead>
+      <tr>
+        <th>Course</th>
+        <th>Duration</th>
+        <th>Cost</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Full Stack</td>
+        <td>3months</td>
+        <td>30K</td>
+      </tr>
+      <tr>
+        <td>Devoops</td>
+        <td>2.5mnths</td>
+        <td>50days</td>
+      </tr>
+    </tbody>
+  </table>
+     <h2>Contact</h2>
+     <p>Email: <a href="mailto:example@example.com">example@example.com</a></p>
+
+     <h2>Links</h2>
+  <ul>
+    <li><a href="https://www.example.com">Visit Example</a></li>
+    <li><a href="https://www.google.com">Google</a></li>
+  </ul> 
+  <footer>
+    <p>&copy; 2024 My Web Page. All rights reserved</p>
+  </footer>
+
+  <br></br>
+  <label>Choose Wallpaper:</label>
+  <select>
+    <option>Wallpaper-1</option>
+    <option>Wallpaper-2</option>
+    <option>Wallpaper-3</option>
+  </select>
+  </div> 
+  );
+}
+
+export default App;
